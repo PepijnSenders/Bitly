cbed43054eee6738107a5e3e49a1d0ec4084b36a
Pepijn Senders <p.senders92@gmail.com>
2014-10-06 16:19:53 +0200

    removed you know

--- tests/BitlyTest.php
+++ tests/BitlyTest.php
@@ -7,11 +7,11 @@ use Pep\Bitly\BitlyRequest\BitlyData;
 class BitlyTest extends PHPUnit_Framework_TestCase {
 
   private $testUser = [
-    'client_id' => 'bde6ac738d68e1c3dee7a41369c313250c7046c3',
-    'client_secret' => '37891aa1b46b56b2179a1fda8d3d9af9274f016c',
-    'redirect_uri' => 'http://honor.np-shared.nl/bitly_callback',
-    'username' => 'noprotocol1',
-    'password' => 'XyfqVKmCqynl',
+    'client_id' => '######',
+    'client_secret' => '######',
+    'redirect_uri' => '######',
+    'username' => '######',
+    'password' => '######',
   ];
 
   public function testGetResourceToken() {
